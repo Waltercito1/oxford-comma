cc6f2568171ff484ad2d0a78ee21b2b0be276a05
Walter Aab <aabwalter@yahoo.com>
2020-10-25 16:40:26 -0500

    commit tree 00f045ce1028005534e3f5e3e4abae6dcc1e4015

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+# require 'pry'
 
+def oxford_comma(array)
+    case array.size
+    when 0
+      ""
+    when 1
+      array.first
+    when 2
+      array.join(" and ")
+    else
+      [array[0..-2].join(", "), array.last].join(", and ")
+    end
+
+    # binding.pry
 end
